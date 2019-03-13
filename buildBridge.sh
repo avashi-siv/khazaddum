6c0e09d3b4046a30f2a525d72cbf39ca12ae668a
Adin Vashi <avashi@starry.com>
2019-03-13 17:41:48 -0400

    initial commit
    - bash script to create bridge on Linux device (to be run on remote
    device)
    
    - bash script to drop DHCP ACK packets going across bridge (to be run on
    bridge)

---
+++ buildBridge.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+brctl addbr br0
+
+brctl addif br0 eth1 eth2
+
+
---
+++ dropDHCP.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+tc qdisc del dev eth2 root
+tc qdisc add dev eth2 root handle 1: htb default 10 
+# unclear what's happening here
+tc class add dev eth2 parent 1: classid 1:1 htb rate 2gbit
+
+tc class add dev eth2 parent 1:1 classid 1:10 htb rate 2gbit
+tc class add dev eth2 parent 1:1 classid 1:11 htb rate 2gbit 
+
+tc filter add dev eth2 protocol ip parent 1: pref 1 u32 \
+	match ip protocol 17 0xff \
+	match u8 0x05 0xff at 270 \
+	flowid 1:11 action drop
+
+
+#tc qdisc add dev eth2 parent 1:10 handle 10: fq_code1
+#tc qdisc add dev eth2 parent 1:11 handle 11: fq_code1
