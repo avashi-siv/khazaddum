744ffd0a521b618761f0d06aae47ee4d2f4a2a7f
Adin Vashi <avashi@starry.com>
2019-04-03 15:45:21 -0400

    Add tox, sphinx, logging
    - Mostly boilerplate
    - Add logging to packet dropper
    - Fix tox errors lol

---
+++ khazaddum.rst
@@ -0,0 +1,22 @@
+khazaddum package
+=================
+
+Submodules
+----------
+
+khazaddum.packetdropper module
+------------------------------
+
+.. automodule:: khazaddum.packetdropper
+    :members:
+    :undoc-members:
+    :show-inheritance:
+
+
+Module contents
+---------------
+
+.. automodule:: khazaddum
+    :members:
+    :undoc-members:
+    :show-inheritance:
---
+++ setup.py
@@ -0,0 +1,21 @@
+"""Set this ting up cmon"""
+import os
+
+from setuptools import find_packages
+from setuptools import setup
+
+BASE_DIR = os.path.abspath(os.path.dirname(__file__))
+ABOUT = {}
+with open(os.path.join(BASE_DIR, "khazaddum", "__about__.py")) as f:
+    exec(f.read(), ABOUT) # pylint: disable=exec-used
+
+setup(
+        name=ABOUT["__title__"],
+        version=ABOUT["__version__"],
+        description=ABOUT["__summary__"],
+        url="https://github.com/avashi-siv/khazaddum",
+        packages=find_packages(),
+        install_requires=[
+            "plumbum>=1.6.7, <2.0",
+            ],
+        )
